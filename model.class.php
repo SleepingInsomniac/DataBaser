bda3d87e82b54301b7c5e58593e99b554a7f1414
Alex Clink <code@alexclink.com>
2014-09-26 12:42:52 -0400

    Fixed the save() method for objects with loaded relations

--- model.class.php
+++ model.class.php
@@ -7,40 +7,49 @@ class Model extends Base {
 	static protected $tableName = null; // override this if you want a different table nam than the pluralized version of your class name
 	static protected $columns = ["*"]; // override this for speed performance
 	
-	//relations
+	//relations: [tableName => className]
 	static protected $hasMany = [];
 	static protected $hasOne = [];
 	static protected $manyToMany = [];
 	
+	// override the get function to catch and initialize relation properties
 	function __get($prop) {
 		// lazy load relations
-		if ( isset(static::$manyToMany[$prop]) && !isset($this->$prop) ) {
-			static::manyToMany($prop); // initialize
-		}
+		if ( isset(static::$manyToMany[$prop]) && !isset($this->$prop) ) static::manyToMany($prop); // initialize
 		return parent::__get($prop);
 	}
-	// function __set($prop, $value) {
-	// 	return parent::__set($prop, $value);
-	// }
 	
 	protected function manyToMany($foreignTable) {
+		// get classname defined in the static $manyToMany (k/v) array
 		$className = static::$manyToMany[$foreignTable];
+		// get the proper order of tables as per naming convention for join table.
 		$joint = static::tableJoin(static::tableName(), $foreignTable);
+		// query from the joint table
 		$query = new Query($joint);
-		$query->select([$className::tableName() => $className::$columns]);
+		// select only the columns from requested class
+		$query->select([$foreignTable => $className::$columns]);
+		// join the requested class on the join table based on primary key
 		$query->join("INNER JOIN `{$className::tableName()}` ON `$joint`.`{$className::tableName()}` = `{$className::tableName()}`.`{$className::$primaryKey}`");
-		$query->where("`$joint`.`{$this::tableName()}` = ?", [$this->id]);
+		// limit to the primary key of this table
+		$query->where("`$joint`.`{$this::tableName()}` = ?", [$this->{static::$primaryKey}]);
+		// run the query and get back a 2d array, set to the requested prop
 		$this->$foreignTable = $className::query($query, $query->params);
+		// convert the arrays to the correct class
 		foreach ($this->$foreignTable as &$row) $row = new $className($row);
 	}
 		
 	protected static function passiveProperties() {
-		return [
+		$passProps = [
 			static::$primaryKey,
 			'created_at',
 			'updated_at',
 			'isNew'
 		];
+		
+		// exclude relations...
+		$passProps = array_merge($passProps, array_keys(static::$manyToMany));
+		
+		return $passProps;
 	}
 	
 	static function tableName() {
@@ -153,7 +162,6 @@ class Model extends Base {
 		$query = new Query($tname);
 		$query->update($tname, $this->toArray())->where(static::$primaryKey.' = ?', [$this->{static::$primaryKey}])->limit(1);
 		$result = static::query($query, $query->params);
-		$this->isNew = false;
 		return $this;
 	}
 	
